0fae81e0bf37c80cdefd310cb715fea4d42d3e87
69520 <yan.zhu@bangdao-tech.com>
2025-09-30 09:22:24 +0800

    feat: add application icon to IntelliGuiApp window
    - Loaded application icon from assets and integrated it into the window options.
    - Enhanced user experience by providing a visual representation of the application in the taskbar and window title.

--- intellid-gui/src/lib.rs
+++ intellid-gui/src/lib.rs
@@ -607,8 +607,10 @@ impl IntelliGuiApp {
 
 
 pub fn run() -> eframe::Result<()> {
+    let data = std::fs::read("assets/icon.png").expect("Failed to load icon");
+    let icon = eframe::icon_data::from_png_bytes(&data).expect("Failed to load icon");
     let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_maximized(true),
+        viewport: egui::ViewportBuilder::default().with_maximized(true).with_icon(icon),
         ..Default::default()
     };
     eframe::run_native(
